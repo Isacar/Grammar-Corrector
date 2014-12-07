461458354529b822b4bca2daedc63f54ecb5e581
Isacar Racine <irodrigu@harding.edu>
2014-12-06 21:06:39 -0600

    Initial Commit

---
+++ GrammarChecker/src/grammarChecker/GrammarChecker.java
@@ -0,0 +1,67 @@
+package grammarChecker;
+//Google Spell checker API
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+//Stanford POS Tagger API
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+public class GrammarChecker {
+
+	public static void main(String[] args) {
+		// Initialize the tagger
+        MaxentTagger tagger = new MaxentTagger(
+                "taggers/english-left3words-distsim.tagger");
+ 
+        // The sample string
+        String sample = "The dog saw a man in the park";
+        //List<String> rules = new ArrayList<String>();
+        Map  <String, String> rules = new HashMap<String, String>();
+        //List<rules> 
+        rules.put("S", "NP VBP");
+        rules.put("NP", "DT NN");
+        rules.put("VBP", "VBD NP");
+        
+        
+        // The tagged string
+        String tagged = tagger.tagString(sample);
+        
+        //get sentence in form of tags
+        String s = POSTSentence (tagged);
+      
+        // Output the result
+        System.out.println(s);
+
+	}
+	
+	
+	//Will return the sentence in form of pos tags
+	private static String POSTSentence (String sentence){
+		String POSTs = "";
+		String[] words = sentence.split(" ");
+		String word = new String();
+		
+		for(int i = 0; i < words.length; i++){
+			word = words[i];
+			String[] wordParts = word.split("_");
+			POSTs += wordParts[1] + " ";
+		}
+		
+		return POSTs;
+	}
+	
+	/*** 
+	 * Returns a post sentence with rules applied to it
+	 * @param postSentence
+	 * @return matchedSentence
+	 */
+	private static String matchRules(String postSentence){
+		String matchedSentence = new String();
+		
+		return matchedSentence;
+		
+	}
+
+}
---
+++ GrammarChecker/src/grammarChecker/Rules.java
@@ -0,0 +1,16 @@
+package grammarChecker;
+
+import java.util.List;
+
+public class Rules {
+	
+	List<String> rules;
+	
+	public Rules(List<String> rules) {
+		
+		this.rules = rules;
+	}
+
+	String word = "NN";
+	
+}
